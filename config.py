ea75a2995ed393112fc1b2cfb0c2c308cd83ef47
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-08 21:21:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,11 @@ class Config(object):
     SUPPORT = os.environ.get("SUPPORT")
     BOT_NAME = os.environ.get("bat")
     BOT_USERNAME = os.environ.get("bn")
+
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "")
+    CAPTION = os.environ.get("CAPTION", "@Mo_Tech_YT @Mo_Tech_Group")
+    BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
+    URL_LINK = os.environ.get("LINK", "T.ME/MO_TECH_YT")
+    USERNAME = os.environ.get("USERNAME", "")
